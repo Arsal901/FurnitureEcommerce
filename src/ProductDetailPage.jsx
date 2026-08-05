f733dd4828e36b8d4a99bb330ce876c422bcf7a7
Arsal Saifi <arshusaifi3@gmail.com>
2026-08-05 20:13:48 +0530

    Added new products and responsive fixes

--- src/ProductDetailPage.jsx
+++ src/ProductDetailPage.jsx
@@ -6,7 +6,7 @@ import { FiMinus } from "react-icons/fi";
 import { GoPlus } from "react-icons/go";
 
 import { FaShoppingCart } from "react-icons/fa";
-
+ 
 import axios from "axios"; 
 
 
@@ -259,7 +259,7 @@ const handleConfirmOrder = () => {
 
 // export default PaymentButton;
 
-  return ( 
+  return (  
     <div className="MainProductPage">
       {/* <h1>Product ID : {id}</h1> */}
   <div className="TopedSection">
